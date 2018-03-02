ccead42e3a97beada38ae0442bccd8bf9e9bad86
zhoutao <zhoutao@yingroup.com>
2018-03-02 20:26:10 +0800

    init prop

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <childTest1 :msg="msg"/>
+  </div>
+</template>
+
+<script>
+import childTest1 from './components/childTest1'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: 'data from parent'
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    childTest1
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ChildTest1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    {{msg}}
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'childTest1',
+  props: ['msg'],
+  data () {
+    return {
+      msgTest2: null
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
