98ea0d99fa225911e92c1917294136759736d481
zhoutao <zhoutao@yingroup.com>
2018-03-06 19:43:52 +0800

    init default router

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,10 @@
 const Home = r => require.ensure([], () => r(require('../page/Home')), 'Home')
 const User = r => require.ensure([], () => r(require('../page/User')), 'User')
 export default [{
+  path: '/',
+  redirect: '/home'
+},
+{
   path: '/home',
   component: Home
 }, {
